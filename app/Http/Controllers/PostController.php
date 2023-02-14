27bbec30ed0cdd069683f3bdc6d5cee9b00b7d89
Itsgabriel <gabrielasimbaya82@gmail.com>
2023-02-14 10:02:37 -0500

    Arreglando bug de la tabla notificaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,13 +40,4 @@ class PostController extends Controller
         
     }
 
-    public function markNotification(Request $request)
-    {
-        auth()->user()->unreadNotifications
-                ->when($request->input('id'), function($query) use ($request){
-                    return $query->where('id', $request->input('id'));
-                })->markAsRead();
-        return response()->noContent();
-    }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -57,9 +57,4 @@ Route::get('/spotify-callback', function () {
 // Una vez creas la ruta, andate al archivo POstController
 Route::resource('post', PostController::class);
 
-Route::get('markAsRead', function(){
-    auth()->user()->unreadNotifications->markAsRead();
-    return redirect()->back();
-})->name('markAsRead');
 
-Route::post('/mark-as-read', 'PostController@markNotification')->name('markNotification');
