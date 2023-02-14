310b20c8a2365a5ab779a31af3a5ccbaf2c61ecb
Itsgabriel <gabrielasimbaya82@gmail.com>
2023-02-13 21:08:37 -0500

    Agregando explicacion en el codigo

--- routes/web.php
+++ routes/web.php
@@ -20,31 +20,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+// Comeinza a leer desde aqui !!!!!!!!!!!
+// El bloque de aqui nos direcciona a ingresar la cuenta de twitter para loguearnos
+Route::get('/login-twitter', function () {
+    return Socialite::driver('twitter')->redirect(); // Esta liena es obetenido de la docuemtnacion
 });
 
- 
-Route::get('/login-twitter', function () {
-    return Socialite::driver('twitter')->redirect();
-});
- 
+// Aqui nos retorna la informacion que obtuvo de twitter y nos manda a la siqguente seccion de nuestra pagina
 Route::get('/twitter-callback', function () {
-    $user = Socialite::driver('twitter')->user();
+    $user = Socialite::driver('twitter')->user(); // Obetnermos el ususrio de twitter
  
     $userExist = User::where('external_id', $user->id)->where('external_auth', 'twitter')->first();
 
     if ($userExist) {
-        Auth::login($userExist);
+        Auth::login($userExist); // SI existe nos logueamos 
     } else {
-        $userNew = User::create([
+        $userNew = User::create([ // Si no lo creamos
+            // Todos los datos de abajo los obetenmos de la session de twitter
             'name' => $user->name,
             'email'=> $user->email,
             'avatar'=>$user->avatar,
             'external_id'=>$user->id,
-            'external_auth'=>'google'
+            'external_auth'=>'twitter'
+            // Una vez creado el ussuraio guarda la informacion en la base de datos
         ]);
         Auth::login($userNew);
     }
     return redirect('/form');
 });
+
+// POR ULTIMO: No olvidar rellenar los camos de avatar, external id y auth en el modelo User en la propiedad fillable
